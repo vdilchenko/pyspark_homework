efbebf8462351c5cb850fa8a7fc2077f91c0f3dc
ikiselev <ikiselev7@gmail.com>
2020-12-06 20:31:40 +0300

    Test travis CI

--- homework/illegal_char/illegal_char_remover.py
+++ homework/illegal_char/illegal_char_remover.py
@@ -9,5 +9,4 @@ class IllegalCharRemover:
         self.replacement = replacement
 
     def remove_illegal_chars(self, dataframe: DataFrame, source_column: str, target_column: str):
-        return dataframe.withColumn(target_column, regexp_replace(col(source_column), self.regex, self.replacement))\
-            .drop(col(source_column))
+        return dataframe.withColumn(target_column, regexp_replace(col(source_column), self.regex, self.replacement))
