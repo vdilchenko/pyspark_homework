74601d86017a8506b82fcbc217f0f77801c5c362
vdilchenko <43196454+vdilchenko@users.noreply.github.com>
2021-01-27 13:50:09 +0300

    UnpackNestedFields

--- homework/unpack_nested_fields/unpack_nested_fields.py
+++ homework/unpack_nested_fields/unpack_nested_fields.py
@@ -1,4 +1,6 @@
-from pyspark.sql.dataframe import DataFrame
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import explode, col
+from pyspark.sql.types import StructType, ArrayType
 
 
 class UnpackNestedFields:
@@ -6,9 +8,31 @@ class UnpackNestedFields:
     Class provides possibilities to unpack nested structures in row recursively and provide flat structure as result.
     To clarify rules, please investigate tests.
     After unpacking of structure additional columns should be provided with next name {struct_name}.{struct_field_name}
-
     """
 
     # ToDo implement unpacking of nested fields
     def unpack_nested(self, dataframe: DataFrame):
-        pass
+        def flatten_struct(schema, prefix=""):
+            result = []
+
+            for elem in schema:
+                if isinstance(elem.dataType, StructType):
+                    result += flatten_struct(
+                        elem.dataType, prefix + elem.name + '.')
+                elif isinstance(elem.dataType, ArrayType):
+                    result.append(explode(col(prefix + elem.name)).alias(
+                            prefix.replace('.', '_') + elem.name))
+                else:
+                    if elem.name.startswith('_'):
+                        result.append(col(prefix + elem.name).alias(
+                            prefix.replace('.', '_') + elem.name))
+                    else:
+                        result.append(col(prefix + elem.name).alias(
+                            prefix + elem.name))
+            return result
+
+        flat_df = dataframe.select(flatten_struct(dataframe.schema))
+        if flat_df.collect() == dataframe.collect():
+            return dataframe
+        else:
+            return flat_df
