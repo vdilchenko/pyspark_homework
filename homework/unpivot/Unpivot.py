23f4b87768106e5005bf63d0629d27e71d008096
vdilchenko <43196454+vdilchenko@users.noreply.github.com>
2021-01-27 13:51:42 +0300

    Unpivot

--- homework/unpivot/Unpivot.py
+++ homework/unpivot/Unpivot.py
@@ -1,5 +1,4 @@
 from typing import List
-
 from pyspark.sql import DataFrame
 
 
@@ -13,10 +12,8 @@ class Unpivot:
     |  1| Ivan|  0.1|  0.1|  0.7|
     |  2|Maria|  0.2|  0.5|  0.9|
     +---+-----+-----+-----+-----+
-
     if we will consider `id` and `name` as constant columns, and columns 10.02, 20.02, 28.02 as dates,
     and other values as score it should provide next result:
-
     +---+-----+-----+-----+
     | id| name| date|score|
     +---+-----+-----+-----+
@@ -27,13 +24,32 @@ class Unpivot:
     |  2|Maria|28.02|  0.9|
     |  2|Maria|20.02|  0.5|
     +---+-----+-----+-----+
-
     See spark sql function `stack`.
     """
 
     def __init__(self, constant_columns: List[str], key_col='', value_col=''):
-        pass
+        self.constant_columns = constant_columns
+        self.key_col = key_col
+        self.value_col = value_col
 
     # ToDo: implement unpivot transformation
     def unpivot(self, dataframe: DataFrame) -> DataFrame:
-        pass
+        cols = [colu for colu in dataframe.columns
+                if colu not in self.constant_columns]
+
+        l = []
+        sep = ', '
+        if cols:
+            for column in cols:
+                if '.' in column:
+                    l.append("'{0}', `{0}`".format(column))
+                else:
+                    l.append("'{0}', {0}".format(column))
+            k = sep.join(l)
+            n = len(cols)
+            new_df = dataframe.selectExpr(
+                *self.constant_columns, 'stack({0}, {1}) as ({2}, {3})'
+                .format(n, k, self.key_col, self.value_col))
+            return new_df
+        else:
+            return dataframe
